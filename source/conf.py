6ed4d87ed236f526e653520c50ca513f58c8f17e
JSangaleti <julianosangaleti@alunos.utfpr.edu.br>
2024-11-10 14:10:33 -0300

    Tentativa 2 de fazer o GitHub Pages funcionar corretamente

--- source/conf.py
+++ source/conf.py
@@ -31,7 +31,7 @@ release = '2025'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.todo',
+    'sphinx.ext.githubpages'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -42,7 +42,7 @@ templates_path = ['_templates']
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = 'pt'
+language = 'pt_BR'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.

ec6f32849b62c8682971d3d6c948d887a17ae418
JSangaleti <julianosangaleti@alunos.utfpr.edu.br>
2024-11-10 14:21:48 -0300

    Tentativa 2 de fazer o GitHub Pages funcionar corretamente

--- source/conf.py
+++ source/conf.py
@@ -64,8 +64,3 @@ html_static_path = ['_static']
 
 
 # -- Extension configuration -------------------------------------------------
-
-# -- Options for todo extension ----------------------------------------------
-
-# If true, `todo` and `todoList` produce output, else they produce nothing.
-todo_include_todos = True
